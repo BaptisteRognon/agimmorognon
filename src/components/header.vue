79c98366741be4a5883382f31bdc9a36f7c08c98
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-05-20 10:15:28 +0200

    etape terminer template header

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,5 +2,24 @@
     
 <div class="bg-indigo-50">
     <div class="flex flex-grow-0 flex-shrink-0 bg-white py-3">
-      
+      <div class="font-inter flex-grow space-x-3">
+        <nav class="hidden lg:flex lg:gap-8">
+          <ul class="flex-grow flex space-x-7 text-base font-medium mt-3 ml-11">
+            <li>
+              <router-link to="/">Rent</router-link>
+            </li>
+            <li>
+              <router-link to="/">Buy</router-link>
+            </li>
+            <li>
+              <router-link to="/">Sell </router-link>
+            </li>
+            <li>
+              <router-link to="/">Manage Property</router-link>
+            </li>
+            <li>
+              <router-link to="/">Resources</router-link>
+            </li>
+          </ul>
+        </nav>
 </template>
